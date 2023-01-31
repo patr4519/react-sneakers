cdbf867334997f53d10cf61216cc2dfd8aa000f9
patr4519 <jero96@mail.ru>
2023-01-31 21:55:48 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 function App() {
   return (
-    <div className="wrapper">
-      <header>
-        <div className="headerLeft">
-          <svg />
-          <div className="headerInfo">
-            <h3>React Sneakers</h3>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/img/logo.png" />
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
             <p>Магазин лучших кроссовок</p>
           </div>
         </div>
-        <ul className="headerRight">
-          <li>
-            <svg />
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={18} height={18} src="/img/cart.svg" />
             <span>1205 руб.</span>
           </li>
           <li>
-            <svg />
+            <img width={18} height={18} src="/img/user.svg" />
           </li>
         </ul>
       </header>
-      <div className="content">
+      <div className="content p-40">
         <h1>Все кроссовоки</h1>
-        ...
+        .....
       </div>
     </div>
   );
