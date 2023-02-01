d5c12d90d574d2e15246cccc4ae06952b605d7ab
patr4519 <jero96@mail.ru>
2023-02-01 11:16:51 +0300

    42.58 time, lesson 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2>Корзина</h2>
+
+          <div className="cartItem d-flex align-center">
+            <img className="mr-20" width={70} height={70} src="/img/sneakers/1.jpg" alt="sneakers" />
+            <div className="mr-20">
+              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img src="/img/btn-remove.svg" alt="remove" />
+          </div>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" />
@@ -20,9 +35,20 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
-        <h1>Все кроссовоки</h1>
+
+        <div className="d-flex align-center mb-40 justify-between">
+          <h1>Все кроссовоки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
+
         <div className="d-flex">
           <div className="card">
+            <div className="favorit">
+              <img src="/img/heart-unliked.svg" alt='Unliked' />
+            </div>
             <img width={133} height={120} src="/img/sneakers/1.jpg" alt="" />
             <p>Мужские Кроссовки Nike Air Max 270</p>
             <div className="d-flex justify-between align-center">
