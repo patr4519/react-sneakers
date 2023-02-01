1966a17daad181a48c31a091029f66cda803a25d
patr4519 <jero96@mail.ru>
2023-02-01 13:38:15 +0300

    1.00 time, lesson 2

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h2>Корзина</h2>
+          <h2 className="mb-30">Корзина</h2>
 
-          <div className="cartItem d-flex align-center">
-            <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"></div>
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }} className="cartItemImg"></div>
 
-            <div className="mr-20">
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-              <b>12 999 руб.</b>
+              <div className="mr-20" flex>
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="remove" />
             </div>
-            <img className="removeBtn" src="/img/btn-remove.svg" alt="remove" />
           </div>
+
+          <ul className="cartTotalBlock">
+            <li>
+              <span>Итого: </span>
+              <div></div>
+              <b>21 498 руб.</b>
+            </li>
+            <li>
+              <span>Налог 5%:</span>
+              <div></div>
+              <b>1 074 руб.</b>
+            </li>
+          </ul>
         </div>
       </div>
 
