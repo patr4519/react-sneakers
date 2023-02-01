28af3ba2d3ee5654c67d62dc537ecae7417cf0c6
patr4519 <jero96@mail.ru>
2023-02-01 21:30:09 +0300

    changed some styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
-arr = [
+let arr = [
   {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999},
   {name: 'Мужские Кроссовки Nike Air Max 270', price: 15999}
 ]
@@ -16,7 +16,7 @@ function App() {
       <div className="content p-40">
 
         <div className="d-flex align-center mb-40 justify-between">
-          <h1>Все кроссовоки</h1>
+          <h1>Все кроссовки</h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
             <input placeholder="Поиск..." />
@@ -24,7 +24,9 @@ function App() {
         </div>
 
         <div className="d-flex">
-          
+          <Card />
+          <Card />
+          <Card />
         </div>
 
       </div>
