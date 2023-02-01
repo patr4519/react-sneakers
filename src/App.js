c72d7ef0d99885c30d777a2e0165a6ee26976c5b
patr4519 <jero96@mail.ru>
2023-02-01 23:01:31 +0300

    1.51.50 time, lesson 3

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
 let arr = [
-  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999},
-  {name: 'Мужские Кроссовки Nike Air Max 270', price: 15999}
+  {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg'},
+  {title: 'Мужские Кроссовки Nike Air Max 270', price: 15999, imageUrl: '/img/sneakers/2.jpg'},
+  {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/3.jpg'},
+  {title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/sneakers/4.jpg'},
 ]
 
 function App() {
@@ -24,9 +26,14 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
+          {
+            arr.map((obj) => {
+              return <Card 
+              title={obj.title} 
+              price={obj.price} 
+              imageUrl={obj.imageUrl}/>
+            })
+          }
         </div>
 
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
-function Card() {
+function Card({title, price, imageUrl}) {
     return (
         <div className="card">
             <div className="favorit">
                 <img src="/img/heart-unliked.svg" alt='Unliked' />
             </div>
-            <img width={133} height={120} src="/img/sneakers/1.jpg" alt="" />
-            <p>Мужские Кроссовки Nike Air Max 270</p>
+            <img width={133} height={120} src={imageUrl} alt="" />
+            <p>{title}</p>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                     <span>Цена:</span>
-                    <b>12 999 руб.</b>
+                    <b>{price} руб.</b>
                 </div>
                 <button className="button">
                     <img width={11} height={11} src="/img/plus.svg" alt="plus" />
