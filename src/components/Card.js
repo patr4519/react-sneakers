175f80bd6a8724284855bc45ab044180ec9bddae
patr4519 <jero96@mail.ru>
2023-02-04 20:48:33 +0300

    1,17,16 time, lesson 5

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function Card({title, price, imageUrl, obj, addToCart}) {
     const [isAdded, setIdAdded] = React.useState(true);
+    const [isFavorite, setIsFavorite] = React.useState(true);
 
     const handlePlus = () => {
         setIdAdded((prev) => !prev);
@@ -11,7 +12,7 @@ function Card({title, price, imageUrl, obj, addToCart}) {
     return (
         <div className="card">
             <div className="favorit">
-                <img src="/img/heart-unliked.png" alt='Unliked' />
+                <img onClick={() => setIsFavorite(!isFavorite)} src={isFavorite ? 'img/unliked.png' : 'img/liked.svg'} alt='Unliked' />
             </div>
             <img width={133} height={120} src={imageUrl} alt="" />
             <p>{title}</p>
